73b5ba3c04097207d4c0e8a3baf0536ee20f7936
João Victor Davim <joaovictor_rj@poli.ufrj.br>
2018-05-08 09:59:07 -0300

    Add bissecao algorithm
    Add bissecao algorithm

--- lu.py
+++ lu.py
@@ -21,24 +21,3 @@ def lu(matrixA):
                     (matrixLU[i, k] * matrixLU[k, j])
 
     return matrixLU
-
-
-# ------------------------------------- main ------------------------------------- #
-
-
-
-# inputs
-a = np.array([[4.5, 2.5, 1.5, 0.5, 1.0, 0.5], [2.5, 5.0, 2.5,
-                                               1.5, 0.5, 1.0], [1.5, 2.5, 4.5, 2.5, 0.5, 1.0], [0.5, 1.5, 2.5, 3.0, 0.5, 1.0], [1.0, 0.5, 0.5, 0.5, 2.5, 1.5], [0.5, 1.0, 1.0, 1.0, 1.5, 2.0]])
-print("\nmatriz de input A:\n" + str(a) + "\n")
-b = np.array([10.0, 20.0, 30.0, 40.0, 30.0, 10.0])
-print("\nmatriz de input b:\n" + str(b) + "\n")
-
-# decomposicao lu
-LU = lu(a)
-print("\nresultado da decomposicao LU:\n" + str(LU) + "\n")
-
-# Resultado de Ax = b
-x = solveLinear(LU, b)
-# print("\nvetor resultado x:\n" + str(x) + "\n")
-# print("\nmultiplicacao Ax (tem que ser igual a b):\n" + str(np.dot(a, x)) + "\n")
