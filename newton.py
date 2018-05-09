a650dd41e7e0ab8063af16d2feda8208ebf2efba
João Victor Davim <joaovictor_rj@poli.ufrj.br>
2018-05-09 11:18:57 -0300

    Add secant algorithm
    Need do number of iterations stop condition.

--- newton.py
+++ newton.py
@@ -12,7 +12,19 @@ def newtonOrig(x0=0, tol=10e-4):
 
 
 def newtonSec(x0=0, tol=10e-4):
-    return
+    tolk = 1
+    Dx = 0.001
+    px = x0
+    x = x0 + Dx
+    fa = f(x0)
+    while tolk > tol:
+        fi = f(x)
+        nx = x - fi*(x - px) / (fi - fa)
+        tolk = abs(nx - x)
+        px = x
+        x = nx
+        fa = fi
+    return x
 
 
 # ------------------------------------- main ------------------------------------- #
@@ -25,6 +37,10 @@ def fl(x):
 x0 = 10
 tol = 5*10e-4
 
-# newton
+# newton original
 x = newtonOrig(x0, tol)
 print ("\nRaiz da funcao f(x) por Newton Original:\n" + str(x) + "\n")
+
+# newton secante
+x = newtonSec(x0, tol)
+print ("\nRaiz da funcao f(x) por Newton Secante:\n" + str(x) + "\n")
