2151f7ad5fe654cfb5282879bcbf7016643cad7c
João Victor Davim <joaovictor_rj@poli.ufrj.br>
2018-05-17 09:15:47 -0300

    Add newton method algorithm
    Add newton method algorithm and remove some "prints" of solveLinear aux function.

--- aux.py
+++ aux.py
@@ -38,7 +38,6 @@ def solveLinear(matrixLU, vectorb):
         for j in range(i):
             firstSum += matrixL[i, j] * vectorY[j]
         vectorY[i] = (vectorb[i] - firstSum) / matrixL[i, i]
-    print("\nVector Y:\n" + str(vectorY) + "\n")
 
     # Substituicao para tras
     vectorx[len(vectorx)-1] = vectorY[len(vectorx)-1] / matrixU[len(vectorx)-1, len(vectorx)-1]
@@ -47,8 +46,6 @@ def solveLinear(matrixLU, vectorb):
         for j in range(i+1, len(vectorx)):
             secondSum += matrixU[i, j] * vectorx[j]
         vectorx[i] = (vectorY[i] - secondSum) / matrixU[i, i]
-    print("\nVector x:\n" + str(vectorx) +"\n")
-    print("\nMultiplicacao Ux (tem que ser igual a Y):\n" + str(np.dot(matrixU,vectorx)) + "\n")
 
     return vectorx
 
---
+++ newtonmethod.py
@@ -0,0 +1,34 @@
+from aux import np, solveLinear
+from lu import lu
+
+
+def newtonmethod(x0, tol=10e-7):
+    tolk = 1
+    vectorpx = x0
+
+    while tolk>tol:
+        matrixJ = Jacob(vectorpx)
+        vectorF = np.array([[f1(vectorpx)], [f2(vectorpx)]])
+        vectorDx = solveLinear(lu(matrixJ), np.dot(-1, vectorF))
+        vectorx = vectorpx + vectorDx
+        tolk = np.linalg.norm(vectorDx) / np.linalg.norm(vectorx)
+        vectorpx = vectorx
+        
+    return vectorx
+
+
+# ------------------------------------- main ------------------------------------- #
+
+# inputs
+x0 = [2, 3]
+def f1(x):
+    return x[0] + 2*x[1] - 2
+def f2(x):
+    return (x[0])**2 + 4*(x[1])**2 - 4
+
+def Jacob(x):
+    return np.array([[1, 2], [2*x[0], 8*x[1]]])
+
+# metodo de newton
+x = newtonmethod(x0)
+print ("\nResultado do sistema pelo metodo de Newton:\n" + str(x) + "\n")
